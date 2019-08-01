55b2ad5a6d028ef41c4056dbfe22006e1c34449c
Nathan L Smith <nsmith@newrelic.com>
2019-07-31 21:58:04 -0500

    non-gql sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import Pokemon from "./Pokemon";
+import React from "react";
 
 const App: React.FC = () => {
-  return (
-    <main></main>
+  const [searchQuery, setSearchQuery] = React.useState("");
+  const handleChange = React.useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) =>
+      setSearchQuery(event.target.value),
+    []
   );
-}
+
+  return (
+    <>
+      <header>
+        <h1>Pokedex</h1>
+        <input
+          autoFocus
+          defaultValue={searchQuery}
+          onChange={handleChange}
+          type="search"
+          placeholder="Search Pokemon by name"
+        />
+      </header>
+      <main>{searchQuery && <Pokemon name={searchQuery} />}</main>
+    </>
+  );
+};
 
 export default App;
